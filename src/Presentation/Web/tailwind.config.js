4db40028768b0464f66cedf00579a936abc57371
Jens Christensen <paviln@outlook.dk>
2023-12-13 01:54:54 +0100

    Add setup

---
+++ src/Presentation/Web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Pages/**/*.cshtml',
+    './Components/**/*.cshtml',
+  ],
+	theme: {
+    extend: {},
+  },
+  plugins: [],
+}
